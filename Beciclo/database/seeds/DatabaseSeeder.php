32c51ef18f0076bc04b48d11b2038c9169820fcf
Oleksandr <ovynokur@jaumebalmes.net>
2015-05-08 17:56:49 +0200

    new
    asdasd

---
+++ Beciclo/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Model::unguard();
+
+		$this->call('UsuarisTableSeeder');
+		$this->call('ProductesTableSeeder');
+		$this->call('ComentarisTableSeeder');
+		$this->call('PreferitsTableSeeder');
+
+		// $this->call('UserTableSeeder');
+	}
+
+}
